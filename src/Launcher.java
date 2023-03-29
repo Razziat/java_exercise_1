816d8693787fd673913b1bf56b0323c0a0f659e3
henri.thomas <henri.thomas@epita.fr>
2023-03-29 11:40:43 +0200

    ex3 loop in launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,18 +1,18 @@
-import java.nio.file.Files;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.stream.Collectors;
 import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Welcome");
         Scanner console = new Scanner(System.in);
-        String name = console.nextLine();
-        if (!name.equals("quit")) {
-            System.out.println("Unknown command");
+        while (true)
+        {
+            String name = console.nextLine();
+            if (name.equals("quit"))
+                break;
+            else {
+                System.out.println("Unknown command");
+            }
         }
+
     }
 }
